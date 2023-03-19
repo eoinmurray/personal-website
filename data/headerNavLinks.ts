7564f3730ceab6d24873689fadd315539b57626a
Eoin Murray <me@eoinmurray.eu>
2023-03-19 17:06:54 +0100

    init

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/articles', title: 'Articles' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
